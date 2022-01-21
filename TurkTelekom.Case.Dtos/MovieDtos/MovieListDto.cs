1c4d6e2a2dfdfc1caa2d82d6ea3e1ff00b86d871
kenankamil <44848981+kenankamil@users.noreply.github.com>
2022-01-21 17:33:34 +0300

    Add files via upload

---
+++ TurkTelekom.Case.Dtos/MovieDtos/MovieListDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurkTelekom.Case.Dtos.GenresDtos;
+using TurkTelekom.Case.Dtos.Interfaces;
+
+namespace TurkTelekom.Case.Dtos.MovieDtos
+{
+    public class MovieListDto : IDto
+    {
+        public int id { get; set; }
+        public List<int> genre_ids { get; set; }
+        public List<GenresListDto> genres { get; set; }
+        public string original_title { get; set; }
+        public string name { get; set; }
+        public string original_language { get; set; }
+        public string title { get; set; }
+        public int vote_count { get; set; }
+        public string poster_path { get; set; }
+        public string backdrop_path { get; set; }
+        public float vote_average { get; set; }
+        public string overview { get; set; }
+        public int runtime { get; set; }
+    }
+}
